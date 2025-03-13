c53db011fc25a3f0402a361925442cfe90ab9e3c
kenn <kennnguyen@usf.edu>
2025-03-13 12:48:45 -0400

    task 4 - risk categorization

--- cc_15.js
+++ cc_15.js
@@ -18,6 +18,22 @@ function addRiskItem(riskName, riskLevel, department) {
 const resolveButton = riskCard.querySelector(".resolveButton");
 resolveButton.addEventListener("click", function ());
 }
+// task 4 categorizing risks by level
+function updateRiskCardStyle(riskCard, riskLevel) {
+    switch (riskLevel.toLowerCase()) {
+        case "low":
+            riskCard.style.backgroundColor = "green";
+            break;
+        case "medium":
+            riskCard.style.backgroundColor = "yellow";
+            break;
+        case "high":
+            riskCard.style.backgroundColor = "red";
+            break;
+        default:
+            riskCard.style.backgroundColor = "white";
+    }
+}
 // task 2 adding risk items dynamically
 addRiskItem(riskName, riskLevel, department);
 document.getElementById("riskForm").reset();
